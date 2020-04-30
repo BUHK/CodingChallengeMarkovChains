70554516f278377d29af1795dfadbdb806fcb317
Matt Moore <zmjm1320@gmail.com>
2020-04-30 12:57:43 -0400

    add specification outline

--- MarkovChainsConsole/Program.cs
+++ MarkovChainsConsole/Program.cs
@@ -1,12 +1,26 @@
 ﻿using System;
 
+
 namespace MarkovChainsConsole
 {
     class Program
     {
+        static string text = "";
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            text = "This rainbow has a unicorn flying over it";
+
+            //parse the text into n-grams
+
+            //calculate probability of each n-gram
+
+            //randomly generate new text based on probability
+
+
+
             Console.ReadLine();
         }
     }
