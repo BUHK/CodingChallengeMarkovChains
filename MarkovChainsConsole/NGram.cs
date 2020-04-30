c08b244cf3856f546e5f890eff5ae2824d84a99c
Matt Moore <zmjm1320@gmail.com>
2020-04-30 16:46:25 -0400

    add n-gram generation functionality

---
+++ MarkovChainsConsole/NGram.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+
+namespace MarkovChainsConsole
+{
+    class NGram : IEquatable<NGram>
+    {
+        public string ThisGram { get; set; }
+        private List<string> PostGrams { get; set; }
+
+        public NGram (string str)
+        {
+            ThisGram = str;
+            PostGrams = new List<string>();
+        }
+
+        public void AddPostGram(string str)
+        {
+            PostGrams.Add(str);
+        }
+
+        public override string ToString()
+        {
+            string str = ThisGram + ":";
+
+            foreach (var item in PostGrams)
+            {
+                str += $" {item},";
+            }
+
+            return str;
+        }
+
+        //public override bool Equals(object other)
+        //{
+        //    NGram otherGram = other as NGram;
+
+        //    return ThisGram.Equals(otherGram.ThisGram);
+        //}
+
+        public bool Equals(NGram other)
+        {
+            return ThisGram.Equals(other.ThisGram);
+        }
+    }
+}
--- MarkovChainsConsole/Program.cs
+++ MarkovChainsConsole/Program.cs
@@ -1,27 +1,82 @@
 ﻿using System;
-
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
 
 namespace MarkovChainsConsole
 {
     class Program
     {
+        //the text to perform work on
         static string text = "";
+
+        static int nGramLength = 3;
+        static int addLength = 1;
+
+        static List<NGram> nGrams;
 
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
 
-            text = "This rainbow has a unicorn flying over it";
+            text = "the theremin is theirs, ok? yes, it is. this is a theremin.";
 
-            //parse the text into n-grams
+            SanitizeText();
 
-            //calculate probability of each n-gram
+            GenerateNGrams();
 
-            //randomly generate new text based on probability
+            foreach (var item in nGrams)
+            {
+                Console.WriteLine(item);
+            }
 
-
+            //MarkovIt();
 
             Console.ReadLine();
+        }
+
+        /**
+         * generate new text
+         */
+        private static void MarkovIt()
+        {
+            throw new NotImplementedException();
+        }
+
+        /**
+         * parse the text into n-grams
+         */
+        private static void GenerateNGrams()
+        {
+            nGrams = new List<NGram>();
+            for (int i = 0; i <= (text.Length - addLength) - nGramLength; i++)
+            {
+                string str = text.Substring(i, nGramLength);
+                NGram currentGram = new NGram(str);
+
+                //if new, add substring of nGramLength to nGram list
+                if (!nGrams.Contains(currentGram))
+                {
+                    nGrams.Add(currentGram);
+                }
+                //else set current gram to existing gram
+                else
+                {
+                    currentGram = nGrams.Find(x => x.Equals(currentGram));
+                }
+
+                //add next char to next char possibility list
+                currentGram.AddPostGram(text.Substring(i + nGramLength, addLength));
+            }
+        }
+
+        /**
+         * sanitize the input text
+         */
+        private static void SanitizeText()
+        {
+            text = text.Trim();
+            text = text.PadRight(addLength);
         }
     }
 }
